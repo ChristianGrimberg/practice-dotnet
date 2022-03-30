c577d5cac1b49468cac155e57daad892e16ac7d6
Christian Grimberg <christian.grimberg@outlook.com>
2022-03-30 03:09:49 +0000

    :monkey_face: Start practice with Chapter 2.

---
+++ Chapter02/Basics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; // a semicolon indicates the end of a statement
+
+// #error version
+// This is a line comment
+/*
+This is a multi-line
+comment.
+*/
+
+namespace Basics
+{ // an open brace indicates the start of a block
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // a statement
+        }
+    }
+} // a close brace indicates the end of a block
